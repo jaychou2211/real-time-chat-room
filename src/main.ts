825002f359597c5dfe72dc18dc4534f337ad28b5
jaychou2211 <cumonkeyte@gmail.com>
2024-09-28 12:08:49 +0800

    refactor : Enable Redis adapter outside root module( AppModule ) to consume configuration values

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { RedisIoAdapter } from './redis-io.adapter';
+import { RedisConfiguration } from './config/redis.configuration';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const redisConfiguration = app.get(RedisConfiguration);
   const redisIoAdapter = new RedisIoAdapter(app);
-  await redisIoAdapter.connectToRedis();
+  await redisIoAdapter.connectToRedis(redisConfiguration.host, redisConfiguration.port);
 
   app.useWebSocketAdapter(redisIoAdapter);
   await app.listen(3000);
--- src/redis-io.adapter.ts
+++ src/redis-io.adapter.ts
@@ -2,12 +2,16 @@ import { IoAdapter } from '@nestjs/platform-socket.io';
 import { ServerOptions } from 'socket.io';
 import { createAdapter } from '@socket.io/redis-adapter';
 import { createClient } from 'redis';
+import { RedisConfiguration } from './config/redis.configuration';
 
 export class RedisIoAdapter extends IoAdapter {
   private adapterConstructor: ReturnType<typeof createAdapter>;
 
-  async connectToRedis(): Promise<void> {
-    const pubClient = createClient({ url: `redis://redis:6379` });
+  async connectToRedis(
+    host: string,
+    port: number,
+  ): Promise<void> {
+    const pubClient = createClient({ url: `redis://${host}:${port}` });
     const subClient = pubClient.duplicate();
 
     await Promise.all([pubClient.connect(), subClient.connect()]);
